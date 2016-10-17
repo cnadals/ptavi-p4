e09fae318c4ed272c2d364378620641f0647f27f
cnadals <carmen.nadal@gmail.com>
2016-10-17 16:43:33 +0200

    server con clase cambiada

--- server.py
+++ server.py
@@ -7,15 +7,15 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 import socketserver
 import sys
 
-class EchoHandler(socketserver.DatagramRequestHandler):
+class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     """
     Echo server class
     """
 
     def handle(self):
         self.wfile.write(b"Hemos recibido tu peticion")
-        print('Introduzca la IP del cliente: ' + self.client_address[0])
-        print('Introduzca el PUERTO del cliente: ' + str(self.client_address[1]))
+        print('IP del cliente: ' + self.client_address[0])
+        print('PUERTO del cliente: ' + str(self.client_address[1]))
         for line in self.rfile:
             print("El cliente nos manda ", line.decode('utf-8'))
 
