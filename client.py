2ea6d6cca36399451d0ccee5d4ae95e3e7e4448e
cnadals <carmen.nadal@gmail.com>
2016-10-17 17:38:04 +0200

    implementado metodo

--- client.py
+++ client.py
@@ -11,15 +11,15 @@ import socket
 # Pido por pantalla ip, puerto y mensaje
 server = sys.argv[1]
 port = int(sys.argv[2])
-metodo = sys.argv[3]
-if metodo != 'register':
-	sys.exit ("User error")
+metodo = "REGISTER sip:" + sys.argv[3] + "SIP/2.0\r\n\r\n"
+#direccionsip = sys.argv[4]
+#if direccionsip != 'register':
+#	sys.exit("User error")
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect((server, port))
-    print("Enviando:", line)
-    my_socket.send(bytes(line, 'utf-8') + b'\r\n')
+    print("Enviando:", metodo)
     data = my_socket.recv(1024)
     print('Recibido -- ', data.decode('utf-8'))
 
