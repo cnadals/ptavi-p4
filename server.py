079615bbe5b735ab6b7782aa2d8f58712038fd66
cnadals <carmen.nadal@gmail.com>
2016-10-20 12:25:06 +0200

    añado expulsion si expires = 0

--- server.py
+++ server.py
@@ -11,18 +11,20 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     """
     Echo server class
     """
-    # creo mi diccionario
+    # creo mi diccionario vacio
     misdatos = {}
 
     def handle(self):
         #imprimo IP y PUERTO por pantalla
         print('IP del cliente: ' + self.client_address[0])
         print('PUERTO del cliente: ' + str(self.client_address[1]))
-        datos =self.rfile.read().decode('utf-8').split(' ')
+        datos = self.rfile.read().decode('utf-8').split(' ')
         #compruebo si es register. si es, mando OK
         if datos[0] == 'REGISTER':
             self.misdatos[datos[1]] = self.client_address
             self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+            if int(datos[-1]) == 0:  # Compruebo si expires = 0.
+                del self.misdatos[datos[1]]  # Si es = 0 --> fuera.
         print(self.misdatos)
 
 if __name__ == "__main__":
