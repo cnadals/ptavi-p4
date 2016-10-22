c35e0eed3d96da6f3508903014cd5aeb99c5b1b0
cnadals <carmen.nadal@gmail.com>
2016-10-22 13:22:51 +0200

    funciona ej 7

--- client.py
+++ client.py
@@ -9,20 +9,25 @@ import socket
 
 # Constantes. Dirección IP del servidor y contenido a enviar
 # Pido por pantalla ip, puerto y mensaje
-server = sys.argv[1]
-port = int(sys.argv[2])
-metodo = "REGISTER sip:" + sys.argv[3] + ' ' + "SIP/2.0\r\n\r\n"
+#server = sys.argv[1]
+#port = int(sys.argv[2])
+#metodo = sys.argv[3]
 # añado el nuevo valor de line (cambia a "una posicion superior")
-line = sys.argv[4]
+#line = sys.argv[4]
 # añado campo expires
-expires = sys.argv[5]
+#expires = sys.argv[5]
+
+if not len(sys.argv) == 6:
+    sys.exit("Usage: client.py ip puerto register sip_address expires_value")
+_, server, port, metodo, line, expires = sys.argv
+port = int(port)
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect((server, port))
     #compruebo si es register, y doy su "nuevo valor"
-    if metodo == 'REGISTER':
-    	line = metodo + 'Expires: ' + expires + '\r\n'
+    if metodo == 'register':
+    	line = 'REGISTER sip:' + line + ' ' + 'SIP/2.0 200 OK\r\n\r\n' + 'Expires: ' + expires + '\r\n'
     print("Enviando:", line)
     my_socket.send(bytes(line, 'utf-8') + b'\r\n')
     data = my_socket.recv(1024)
