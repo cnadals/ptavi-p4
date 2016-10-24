98a0274b3108d84623cd502d7b63393a4e32b726
cnadals <carmen.nadal@gmail.com>
2016-10-24 18:37:52 +0200

    FIN cliente

--- client.py
+++ client.py
@@ -7,16 +7,6 @@ Programa cliente UDP que abre un socket a un servidor
 import sys
 import socket
 
-# Constantes. Dirección IP del servidor y contenido a enviar
-# Pido por pantalla ip, puerto y mensaje
-#server = sys.argv[1]
-#port = int(sys.argv[2])
-#metodo = sys.argv[3]
-# añado el nuevo valor de line (cambia a "una posicion superior")
-#line = sys.argv[4]
-# añado campo expires
-#expires = sys.argv[5]
-
 # Compruebo la longitud de los argumentos
 # Creo excepcion: si no esta bien escrito, cómo se hace.
 # Para ello, elimino los sys anteriores.
@@ -25,15 +15,12 @@ if not len(sys.argv) == 6:
 _, server, port, metodo, line, expires = sys.argv
 port = int(port)
 
-# Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect((server, port))
-    # compruebo si es register, y doy su "nuevo valor"
     if metodo == 'register':
-    	line = 'REGISTER sip:' + line + ' ' + 'SIP/2.0 200 OK\r\n\r\n' + 'Expires: ' + expires + '\r\n'
+        line = 'REGISTER sip:' + line + ' ' + 'SIP/2.0\r\n\r\n' + 'Expires: ' + expires + '\r\n'
     print("Enviando:", line)
     my_socket.send(bytes(line, 'utf-8') + b'\r\n')
     data = my_socket.recv(1024)
     print('Recibido -- ', data.decode('utf-8'))
-
 print("Socket terminado.")
